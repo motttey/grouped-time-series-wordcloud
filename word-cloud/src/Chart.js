9f007c0e0dbf59c85a5cf18621c6d8d9eb410ef0
motttey <motitago@gmail.com>
2021-01-25 22:06:17 +0900

    show leave texts without calling nested function

--- word-cloud/src/Chart.js
+++ word-cloud/src/Chart.js
@@ -7,7 +7,7 @@ function Chart(props) {
   const height = 800;
   const margin = { top: 50, right: 50, bottom: 50, left: 50 };
   const marginSparkLine = 10;
-  const fontSizeMax = 30;
+  const fontSizeMax = 100;
   const transitionMax = 1000;
 
   const svg = d3.select("svg")
@@ -38,7 +38,7 @@ function Chart(props) {
         })
     };
 
-    const root = d3.hierarchy(data_reduced);
+    const root = d3.hierarchy(data);
     root.sum((d) => d.size);
 
     const treemap = d3.treemap()
@@ -48,8 +48,11 @@ function Chart(props) {
 
     treemap(root);
 
+    const parentNames = data.children.map((d) => d.word);
+    const childLeaves = root.leaves();
+
     const updateRect = svg.select("g#treemap").selectAll("rect")
-      .data(root.leaves());
+      .data(childLeaves);
 
     const enterRect = updateRect
       .enter().append("rect")
@@ -63,11 +66,39 @@ function Chart(props) {
       })
       .attr("width", (d) => d.x1 - d.x0)
       .attr("height", (d) => d.y1 - d.y0)
-      .style("fill", (_, i) => d3.schemeCategory10[i])
+      .style("fill", (d, i) =>  {
+        return d3.schemeCategory10[parentNames.indexOf(d.parent.data.word)];
+      })
       .style("opacity", 0.6)
       .each((d, i) => {
-        drawWordCloudFromTreemap(data, d, i);
+        // drawWordCloudFromTreemap(data, d, i);
       });
+
+    const updateText = svg.select("g#treemap").selectAll("text")
+      .data(childLeaves);
+
+    const enterText = updateText
+      .enter().append("text")
+      .attr("class", "text");
+
+    enterText.merge(updateText)
+      .transition()
+      .duration(transitionMax)
+      .style("font-size", function (d) {
+        return d3.min([d.data.size, fontSizeMax]).toString() + "px";
+      })
+      .attr("fill", (d) => d3.schemeCategory10[
+        parentNames.indexOf(d.parent.data.word)
+      ])
+      .attr("text-anchor", "middle")
+      .style("font-family", "Impact")
+      .attr("transform", function (d) {
+        return "translate("
+          + [ d.x0 + (d.x1 - d.x0) / 2 , d.y0 + (d.y1 - d.y0) / 2 ]
+          + ")rotate(" + 0 + ")";
+      })
+      .text((d) => d.data.word);
+
   }
 
   function drawWordCloudFromTreemap(data, treeMapData, groupIndex) {
@@ -115,7 +146,8 @@ function Chart(props) {
         .transition()
         .duration(transitionMax)
         .style("font-size", function (d) {
-          return d3.min([d.size, fontSizeMax]).toString() + "px";
+          console.log(d);
+          return d3.min([d.data.size, fontSizeMax]).toString() + "px";
         })
         .attr("fill", (d) => d3.schemeCategory10[groupIndex])
         .attr("text-anchor", "middle")
