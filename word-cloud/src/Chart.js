9ee40bcd7a655792063b16926b91df3e91a81e31
motttey <motitago@gmail.com>
2021-01-26 22:30:13 +0900

    show line chart on each cell of treemap

--- word-cloud/src/Chart.js
+++ word-cloud/src/Chart.js
@@ -6,7 +6,7 @@ function Chart(props) {
   const width = 800;
   const height = 800;
   const margin = { top: 50, right: 50, bottom: 50, left: 50 };
-  const marginSparkLine = 10;
+  const marginSparkLine =  5;
   const fontSizeMax = 100;
   const transitionMax = 1000;
 
@@ -27,17 +27,6 @@ function Chart(props) {
   drawTreemap(props.data[props.index]);
 
   function drawTreemap(data) {
-    const data_reduced = {
-      word: data.word,
-      children: data.children
-        .map((d) => {
-          return {
-            word: d.word,
-            size: d3.sum(d.children, (c) => Math.abs(c.size))
-          }
-        })
-    };
-
     const root = d3.hierarchy(data);
     root.sum((d) => d.size);
 
@@ -94,11 +83,21 @@ function Chart(props) {
       .style("font-family", "Impact")
       .attr("transform", function (d) {
         return "translate("
-          + [ d.x0 + (d.x1 - d.x0) / 2 , d.y0 + (d.y1 - d.y0) / 2 ]
+          + [ d.x0 + (d.x1 - d.x0) / 2 - marginSparkLine,
+              d.y0 + (d.y1 - d.y0) / 2 - marginSparkLine ]
           + ")rotate(" + 0 + ")";
       })
-      .text((d) => d.data.word);
-
+      .text((d) => d.data.word)
+      .each((d, i) => {
+        const parent = d.parent.data.word;
+        const groupSeries = props.data.map(
+          (v) => v.children[parentNames.indexOf(parent)]
+        )
+        .map(
+          (v) => v.children.find((e) => e.word === d.data.word)
+        );
+        drawLinechart(d, groupSeries, parentNames, parent);
+      });
   }
 
   function drawWordCloudFromTreemap(data, treeMapData, groupIndex) {
@@ -182,66 +181,167 @@ function Chart(props) {
            );
            drawCircles(node[i], timeSeries, groupIndex);
          });
-
     }
+  }
 
-    function drawCircles(node, timeSeries, groupIndex) {
-      const xScale = d3.scaleLinear()
-        .domain([0, timeSeries.length])
-        .range([0, 50]);
+  function drawCircles(node, timeSeries, groupIndex) {
+    const xScale = d3.scaleLinear()
+      .domain([0, timeSeries.length])
+      .range([0, 50]);
 
-      const yScale = d3.scaleLinear()
-        .domain([0, d3.max(timeSeries, (t) => t.size)])
-        .range([5 * timeSeries.length, 0]);
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(timeSeries, (t) => t.size)])
+      .range([5 * timeSeries.length, 0]);
 
-      d3.select(node).select("path")
-        .datum(timeSeries)
-        .attr("fill", "none")
-        .attr("stroke", d3.schemeCategory10[groupIndex])
-        .attr("stroke-width", 1)
-        .attr("d", d3.line()
-          .x((_, i) => xScale(i))
-          .y((t) => yScale(t.size))
-        );
+    d3.select(node).select("path")
+      .datum(timeSeries)
+      .attr("fill", "none")
+      .attr("stroke", d3.schemeCategory10[groupIndex])
+      .attr("stroke-width", 1)
+      .attr("d", d3.line()
+        .x((_, i) => xScale(i))
+        .y((t) => yScale(t.size))
+      );
 
-      const updateCircle = d3.select(node)
-        .selectAll("circle.currentNode")
-        .data(timeSeries);
+    const updateCircle = d3.select(node)
+      .selectAll("circle.currentNode")
+      .data(timeSeries);
 
-      const enterCircle = updateCircle
-        .enter().append("circle")
-        .attr("class", "currentNode");
+    const enterCircle = updateCircle
+      .enter().append("circle")
+      .attr("class", "currentNode");
 
-      enterCircle.merge(updateCircle)
-        .transition(transitionMax)
-        .attr("fill", (_, i) =>
-          (i === props.index)? "orange": d3.schemeCategory10[groupIndex]
-        )
-        .attr("r", (_, i) =>
-          (i === props.index)? 3: 2
-        )
-        .attr("stroke", "black")
-        .attr("stroke-width", (_, i) =>
-          (i === props.index)? 1: 0
-        )
-        .attr("cx", (_, i) => xScale(i))
-        .attr("cy", (t) => yScale(t.size))
+    enterCircle.merge(updateCircle)
+      .transition(transitionMax)
+      .attr("fill", (_, i) =>
+        (i === props.index)? "orange": d3.schemeCategory10[groupIndex]
+      )
+      .attr("r", (_, i) =>
+        (i === props.index)? 3: 2
+      )
+      .attr("stroke", "black")
+      .attr("stroke-width", (_, i) =>
+        (i === props.index)? 1: 0
+      )
+      .attr("cx", (_, i) => xScale(i))
+      .attr("cy", (t) => yScale(t.size))
 
-      d3.select(node).selectAll("circle.currentNode")
-        .on("click", (event, d) => {
-          props.updateIndex(timeSeries.indexOf(d));
-        })
-        .on("mouseover", (event, d) => {
-          d3.select(event.currentTarget)
-            .attr("stroke", "red")
-            .attr("stroke-width", 2);
-        })
-        .on("mouseout", (event, d) => {
-          d3.select(event.currentTarget)
-            .attr("stroke", "black")
-            .attr("stroke-width", 1);
-        });
-    }
+    d3.select(node).selectAll("circle.currentNode")
+      .on("click", (event, d) => {
+        props.updateIndex(timeSeries.indexOf(d));
+      })
+      .on("mouseover", (event, d) => {
+        d3.select(event.currentTarget)
+          .attr("stroke", "red")
+          .attr("stroke-width", 2);
+      })
+      .on("mouseout", (event, d) => {
+        d3.select(event.currentTarget)
+          .attr("stroke", "black")
+          .attr("stroke-width", 1);
+      });
+  }
+
+  function drawLinechart (treeMapData, timeSeries, parentNames, parent) {
+    const targetId = "g#" + parent + "-" + treeMapData.data.word;
+    const treeMapWidth = treeMapData.x1 - treeMapData.x0;
+    const treeMapHeight = treeMapData.y1 - treeMapData.y0;
+
+    svg.select("g#treemap").selectAll(targetId)
+      .data(treeMapData)
+      .enter().append("g")
+      .attr("id", parent + "-" + treeMapData.data.word)
+      .attr("width", treeMapWidth)
+      .attr("height", treeMapHeight)
+      .attr("transform",
+          "translate("
+          + (treeMapData.x0 + treeMapWidth/2) + ","
+          + (treeMapData.y0 + treeMapHeight/2) + ")");
+
+    const updateLineChart = svg.select("g#treemap").selectAll(targetId)
+      .data(treeMapData);
+
+    const enterLineChart = updateLineChart
+      .enter().append("g")
+      .attr("class", parent + "-" + treeMapData.data.word);
+
+    const merged = enterLineChart.merge(updateLineChart);
+
+    merged
+      .transition()
+      .duration(transitionMax)
+      .attr("width", treeMapWidth)
+      .attr("height", treeMapHeight)
+      .attr("transform",
+          "translate("
+          + (treeMapData.x0 + treeMapWidth/2 + marginSparkLine) + ","
+          + (treeMapData.y0 + treeMapHeight/2 + marginSparkLine) + ")");
+
+    const groupIndex = parentNames.indexOf(parent);
+    const xScale = d3.scaleLinear()
+      .domain([0, timeSeries.length])
+      .range([0, 50]);
+
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(timeSeries, (t) => t.size)])
+      .range([5 * timeSeries.length, 0]);
+
+    const updateLine = merged
+      .selectAll("path.timeSeries")
+      .data(timeSeries);
+
+    const enterLine = updateLine
+      .enter().append("path")
+      .attr("class", "timeSeries");
+
+    updateLine.merge(enterLine)
+      .datum(timeSeries)
+      .transition(transitionMax)
+      .attr("fill", "none")
+      .attr("stroke", d3.schemeCategory10[groupIndex])
+      .attr("stroke-width", 1)
+      .attr("d", d3.line()
+        .x((_, i) => xScale(i))
+        .y((t) => yScale(t.size))
+      );
+
+    const updateCircle = merged
+      .selectAll("circle.currentNode")
+      .data(timeSeries);
+
+    const enterCircle = updateCircle
+      .enter().append("circle")
+      .attr("class", "currentNode");
+
+    enterCircle.merge(updateCircle)
+      .transition(transitionMax)
+      .attr("fill", (_, i) =>
+        (i === props.index)? "orange": d3.schemeCategory10[groupIndex]
+      )
+      .attr("r", (_, i) =>
+        (i === props.index)? 3: 2
+      )
+      .attr("stroke", "black")
+      .attr("stroke-width", (_, i) =>
+        (i === props.index)? 1: 0
+      )
+      .attr("cx", (_, i) => xScale(i))
+      .attr("cy", (t) => yScale(t.size))
+
+    merged.selectAll("circle.currentNode")
+      .on("click", (event, d) => {
+        props.updateIndex(timeSeries.indexOf(d));
+      })
+      .on("mouseover", (event, d) => {
+        d3.select(event.currentTarget)
+          .attr("stroke", "red")
+          .attr("stroke-width", 2);
+      })
+      .on("mouseout", (event, d) => {
+        d3.select(event.currentTarget)
+          .attr("stroke", "black")
+          .attr("stroke-width", 1);
+      });
   }
 
   useEffect(() => {
