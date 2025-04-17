f7648b59da0098e6938ffd277efe4ce647065309
motttey <motitago@gmail.com>
2025-04-17 20:03:45 +0900

    modify: run vitest

--- word-cloud/src/App.test.jsx
+++ word-cloud/src/App.test.jsx
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { vi, expect, test } from "vitest";
 
-test('renders learn react link', () => {
+vi.mock('d3');
+
+vi.mock('./Chart', () => ({
+  default: () => <svg id="timelineSvg" />,
+}));
+
+test('renders page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/word-cloud/i);
+  expect(linkElement).not.toBeNull();
 });
