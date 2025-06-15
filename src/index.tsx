1921e1c25497eb6f002cba0a978706551a4188b8
motttey <motitago@gmail.com>
2025-06-15 21:36:02 +0900

    introduce: typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 
-const root = createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Failed to find the root element');
+}
+
+const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
