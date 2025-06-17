43a63ee4e8b22d69162b79f2a760d2670b2635dd
motttey <motitago@gmail.com>
2025-06-17 22:01:44 +0900

    fix: js -> ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.ts';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -19,4 +19,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(undefined);
